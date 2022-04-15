9af7443d99e9559468e278fb8f426e442df3a132
Oliver <oliverudu@gmail.com>
2022-04-15 17:26:13 +0300

    feat: implement image creation

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from datetime import date
 import download_background
+import generate_image
 
 def main():
-    download_background.download_image(download_background.get_random_image_url(date.today()), f"resources/backgrounds/{date.today()}.jpg")
+
+    background_path: str = f"resources/backgrounds/{date.today()}.jpg"
+    download_background.download_image(background_path)
+    instagram_post = generate_image.create_image(65, background_path)
+    instagram_post.save(f"output/{date.today()}.jpg", "JPEG", quality=100)
+
 
 main()
